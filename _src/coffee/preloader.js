226321c138573310153709843d1ec659849bb9aa
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-12-13 10:24:59 +0200

    clean up

--- _src/coffee/preloader.js
+++ _src/coffee/preloader.js
@@ -1,88 +1,62 @@
-window.requestAnimFrame = (function () {
-  return  window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function (callback) {
-      window.setTimeout(callback, 1000 / 60);
-    };
-})();
+var requestAnimFrame = require("animationframe");
 var previousProgress = 0;
-module.exports = function(progress, callback) {
-  progress = Math.ceil(360 * progress);
-  var canvas = document.getElementById('preloader'),
-    width = canvas.width,
-    height = canvas.height,
-    ctx = canvas.getContext('2d'),
-    imd = null;
 
-  var step = -90,
-    startAngle = -90,
-    endAngle = progress+(startAngle*2);
+var canvas = document.getElementById('preloader'),
+  width = canvas.width,
+  height = canvas.height,
+  ctx = canvas.getContext('2d'),
+  progress = 0;
 
-  var x = width / 2, // center x
-    y = height / 2, // center y
-    radius = width / 3,
-    counterClockwise = false;
+var step = 0,
+  startAngle = 0,
+  endAngle = 0;
 
+var x = width / 2, // center x
+  y = height / 2, // center y
+  radius = width / 3,
+  counterClockwise = false;
+
+ctx.imageSmoothingEnabled = true;
+ctx.lineWidth = 0;
+ctx.fillStyle = '#221e1a';
+ctx.strokeStyle = '#221e1a';
+ctx.lineCap = 'square';
+
+function draw() {
+  drawArc(startAngle * Math.PI/180 - 90 * Math.PI/180, step * Math.PI/180 - 90 * Math.PI/180);
+  if (step<endAngle) {
+    step += 4;
+  }
+}
+function drawArc(s, e) {
+  ctx.clearRect(0,0,width, height);
+
+  ctx.fillStyle = 'rgba(34,30,26,.4)';
   ctx.beginPath();
-  ctx.imageSmoothingEnabled = true;
-  ctx.lineWidth = 0;
-  ctx.fillStyle = '#221e1a';
-  ctx.strokeStyle = '#221e1a';
-  ctx.lineCap = 'square';
-  ctx.closePath();
+  ctx.moveTo(x, y);
+  ctx.arc(x, y, radius, s, 360 * Math.PI/180, counterClockwise);
   ctx.fill();
 
-  imd = ctx.getImageData(0, 0, width, height);
+  ctx.fillStyle = '#221e1a';
+  ctx.strokeStyle = '#221e1a';
 
-  function draw() {
-    drawArc(startAngle * Math.PI/180, step * Math.PI/180);
-    step+=4;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.arc(x, y, radius, s, e, counterClockwise);
+  ctx.fill();
+}
+function onEnterFrame() {
+  draw();
+  if (step<360) {
+    requestAnimFrame(onEnterFrame, ctx);
+  } else {
+    callback();
   }
-  function drawArc(s, e) {
-    ctx.clearRect(0,0,width, height);
+}
+onEnterFrame();
 
-    ctx.fillStyle = 'rgba(34,30,26,.4)';
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.arc(x, y, radius, s, 275 * Math.PI/180, counterClockwise);
-    ctx.fill();
-
-    ctx.fillStyle = '#221e1a';
-    ctx.strokeStyle = '#221e1a';
-
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.arc(x, y, radius, s, previousProgress * Math.PI/180, counterClockwise);
-    ctx.fill();
-
-    previousProgress = progress;
-    ctx.putImageData(imd, 0, 0);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.arc(x, y, radius, s, e, counterClockwise);
-    ctx.fill();
-  }
-
-  window.requestAnimFrame = (function () {
-    return  window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      function (callback) {
-        window.setTimeout(callback, 1000 / 60);
-      };
-  })();
-
-  (function onEnterFrame() {
-    draw();
-    if (step <= endAngle-startAngle) {
-      requestAnimFrame(onEnterFrame, canvas);
-    } else if (step>=274) {
-      callback();
-    }
-  }());
+module.exports = function(progress, c) {
+  progress = Math.ceil(360 * progress);
+  endAngle = progress;
+  callback = c;
 };
